8ada5b93f8462ffe03900fb7606bb2cae7b890c4
kylepg <kylepgeorge@gmail.com>
2018-02-06 13:01:47 -0500

    Updated viewport meta data for bootstrap responsiveness

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html>
 
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="utf-8">
     <title> Kyle George - Project 1</title>
     <link rel="icon" href="/images/favicon.png" type="image/x-icon" />
     <link rel="stylesheet" href="/lib/bootstrap/css/bootstrap.min.css">
